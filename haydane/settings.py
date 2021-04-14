4a9744dfbf00acf31341921e2b2d78787bfec265
Hay Dane <haydane@Hays-MacBook-Pro.local>
2021-04-14 21:56:30 +0700

    polls app

--- haydane/settings.py
+++ haydane/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,13 +74,23 @@ WSGI_APPLICATION = 'haydane.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.mysql', 
+    'NAME': 'haydane',
+    'USER': 'haydane',
+    'PASSWORD': 'toor',
+    'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+    'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
